dc4ea3c8b7a34cad52f39797022916648ff7e034
Magnus <97634880+MagnusS0@users.noreply.github.com>
2024-03-13 02:39:50 +0100

    feat: Switched to faster-whisper
    -Faster-wisper uses CTranslate2 infrence engine
    this should result in a good speed up, escpecilly for larger text

--- src/models/nb_whisper_model/load_model.py
+++ src/models/nb_whisper_model/load_model.py
@@ -1,10 +1,15 @@
-# Load the model that can be imported in other scripts
-from transformers import pipeline
+# Load the model from the HuggingFace model hub 
 import torch
+from faster_whisper import WhisperModel
 
-def load_model(model_size='small'):
-    device = 0 if torch.cuda.is_available() else -1
-    model = pipeline("automatic-speech-recognition", f"NbAiLabBeta/nb-whisper-{model_size}", device=device)
+def load_model(model_size='small', compute_type = "float16", device = 'cuda' if torch.cuda.is_available() else 'cpu' ):
+    model = WhisperModel(f"NbAiLab/nb-whisper-{model_size}", 
+                         device=device, 
+                         compute_type=compute_type, 
+                         cpu_threads=4, 
+                         num_workers=6
+                    )
     return model
+
 
 nb_whisper = load_model()
