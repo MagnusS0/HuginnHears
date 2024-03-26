628e7f8489a9e0896b729736f037abe7b9937d08
Magnus <97634880+MagnusS0@users.noreply.github.com>
2024-03-26 19:50:43 +0100

    feat: Update setup.py to fix bug
    Issue with paths is fixed by setting them to relative paths

--- setup.py
+++ setup.py
@@ -5,21 +5,22 @@ from cx_Freeze import setup, Executable
 build_options = {'packages': ['streamlit', 'torch', 'llmlingua','transformers',
                               'llama_cpp','faster_whisper','langchain_openai',
                               'langchain', 'av'], 
-                 'excludes': [],
+                 'excludes': ['ipykernel', 'ipython', 'jupyter_client', 'jupyter_core',],
                  "include_files": [
                      ("streamlit_app/", "streamlit_app/"),
                      ("huginn_hears/", "streamlit_app/huginn_hears/"),
-                     ]
+                     ],
+                 "replace_paths": [("*", "")], # Makes sure all paths are relative so that the app can be run from any directory
                      }
 
-base = 'console'
+base = None
 
 executables = [
     Executable('streamlit_app/run.py', base=base, target_name = 'huginn-hears')
 ]
 
-setup(name='Huginn-hears',
-      version = '1',
+setup(name='HuginnHears',
+      version = '0.1.0',
       description = 'Transcribe speech and summerize in Norwegian',
       options = {'build_exe': build_options},
       executables = executables)
